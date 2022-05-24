01de1f6374cba7e03250797cb7aa8db27a2d3ca8
Aleksandr Samokhin <alexsamohin@gmail.com>
2022-05-25 01:26:14 +0300

    Added model User

--- tg_viewer/viewer/admin.py
+++ tg_viewer/viewer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from viewer.models import (Chat, Message, )
+from viewer.models import (Chat, Message, User)
 
 class ChatAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', )
@@ -8,6 +8,9 @@ class ChatAdmin(admin.ModelAdmin):
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('id', 'chat')
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_id')
+
 admin.site.register(Chat, ChatAdmin)
 admin.site.register(Message, MessageAdmin)
-
+admin.site.register(User, UserAdmin)
--- tg_viewer/viewer/models.py
+++ tg_viewer/viewer/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+class User(models.Model):
+    user = models.CharField(max_length=35, verbose_name='Пользователь')
+    user_id = models.CharField(max_length=35, verbose_name='ID')
+
 class Chat(models.Model):
     id = models.IntegerField(editable=False, primary_key=True)
-    name = models.CharField(max_length=35, verbose_name="Имя")
+    name = models.CharField(max_length=35, verbose_name='Имя')
 
 class Message(models.Model):
     id = models.IntegerField(editable=False, primary_key=True)
-
+    visibility = models.BooleanField(verbose_name='Видимость', default=True)
     chat = models.ForeignKey(Chat, verbose_name='Чат', on_delete = models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete = models.CASCADE, null=True)
